0ce499a0fb726fe232b6653a5338b948b1e6583c
Sebastian Roldan Daza <roldansebasdaza@gmail.com>
2025-04-06 14:20:05 -0500

    ...

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ const cors = require("cors");
 const routes = require("./src/routes");
 
 const app = express();
+const AUTOR = process.env.AUTOR || "SRD";
+const PORT = process.env.PORT || 3000;
+const APP_URL = process.env.APP_URL || 'http://localhost';
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ mensaje: "¡Servidor funcionando correctamente!" });
+app.get('/', (req, res) => {
+  res.json({
+    nombre: 'API de THE BARBER HOUSE',
+    version: '1.0.0',
+    autor: AUTOR,
+    fecha: new Date().toISOString()
+  });
 });
 
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-const APP_URL = process.env.APP_URL || 'http://localhost';
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en ${APP_URL}:${PORT}`);
 });
