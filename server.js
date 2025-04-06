8def76f3e247e325275f4946ed43b1a1784b25f6
Sebastian Roldan Daza <roldansebasdaza@gmail.com>
2025-04-06 14:24:25 -0500

    ,,,

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes");
+const favicon = require('serve-favicon');
+const path = require('path');
 
 const app = express();
 const AUTOR = process.env.AUTOR || "SRD";
@@ -10,6 +12,7 @@ const APP_URL = process.env.APP_URL || 'http://localhost';
 
 app.use(cors());
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => {
   res.json({
