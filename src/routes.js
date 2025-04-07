373dd7f4fd3cc8c1445810fff65485b169087059
Juan-H18 <hernandez.juanjose2005@gmail.com>
2025-04-07 15:39:30 -0500

    A roldan le gusta el pito

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,13 @@ const insumoRoutes = require('./routes/insumo.routes.js');
 const categoriaInsumoRoutes = require('./routes/categoriaInsumo.routes');
 const ventasRoutes = require('./routes/ventas.routes');
 
+const categoriaProductosRoutes = require('./routes/categoriaProductos.routes');
+
+
 router.use('/auth', authRoutes);
 router.use('/insumos', insumoRoutes);
 router.use('/categoria-insumo', categoriaInsumoRoutes);
+router.use('/categoria-producto', categoriaProductosRoutes);
 router.use('/ventas', ventasRoutes);
 
 module.exports = router;
