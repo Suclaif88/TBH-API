c6230dae0957181125777fdab9948f6947c10713
Sebastian Roldan Daza <roldansebasdaza@gmail.com>
2025-04-12 20:20:53 -0500

    ...

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.get('/', (req, res) => {
   res.json({
     nombre: 'API de THE BARBER HOUSE',
-    version: '1.2.0',
+    version: process.env.VERSION || '0.0.0',
     autor: AUTOR,
     fecha: new Date().toISOString()
   });
