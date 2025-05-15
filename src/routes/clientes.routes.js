6858bd25b197c968765971b1a369347fdde07cfa
Randalbr <brandonestivenchavarria977@gmail.com>
2025-05-15 09:27:09 -0500

    Cambois en Cliente

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const {
   crearCliente,
   listarCliente,
-  listarClienteDocumento,
+  listarClientePorId,
   actualizarCliente,
   eliminarCliente
 } = require('../controllers/cliente.controller');
@@ -14,9 +14,9 @@ const router = Router();
 router.use(verificarToken);
 
 router.get('/', listarCliente);
-router.get('/:documento', listarClienteDocumento)
+router.get('/:id_cliente', listarClientePorId)
 router.post('/', crearCliente);
-router.put('/:documento_cliente', actualizarCliente);
-router.delete('/:documento_cliente', eliminarCliente);
+router.put('/:id_cliente', actualizarCliente);
+router.delete('/:id_cliente', eliminarCliente);
 
 module.exports = router;
