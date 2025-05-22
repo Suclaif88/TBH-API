542d0291b588cd6d9e1bff2fba6a6cc98f1d6c9d
Juan-H18 <hernandez.juanjose2005@gmail.com>
2025-05-22 17:04:46 -0500

    Se hizo detalles a medias : (

--- src/routes.js
+++ src/routes.js
@@ -21,6 +21,7 @@ const novedadesRoutes= require ('./routes/novedades.routes.js');
 
 const comprasRoutes = require ('./routes/compras.routes.js');
 const detallesCompraInsumosRoutes = require ('./routes/detalleCompraInsumos.routes.js');
+const detallesCompraProductosRoutes = require ('./routes/detalleCompraInsumos.routes.js');
 const proveedoresRoutes = require('./routes/proveedores.routes.js');
 const categoriaProductosRoutes = require('./routes/categoriaProductos.routes.js');
 const ProductosRoutes = require('./routes/productos.routes.js');
@@ -39,6 +40,8 @@ router.use('/insumos', insumoRoutes);
 
 router.use ('/compras', comprasRoutes);
 router.use ('/detalleCompraInsumos', detallesCompraInsumosRoutes);
+router.use ('/detalleCompraProductos', detallesCompraProductosRoutes);
+
 router.use('/proveedores', proveedoresRoutes);
 router.use('/productos', ProductosRoutes);
 router.use('/producto-imagen', ProductoImagenRoutes);
