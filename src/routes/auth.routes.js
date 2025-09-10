0bc15fdc2d659af166a2d082694be38b1bb71aec
Randalbr <brandonestivenchavarria977@gmail.com>
2025-09-10 10:06:47 -0500

    Enail,Landing

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { register,
-        login } = require('../controllers/auth.controller');
+        login,
+        activate,
+        resetPassword,
+        forgotPassword,
+     } = require('../controllers/auth.controller');
 const { loginLimiter, registerLimiter } = require('../middleware/rateLimiters');
 
 router.post('/register', registerLimiter, register);
 router.post('/login', loginLimiter, login);
+
+
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
+
+router.get('/activate/:token', activate);    
 
 module.exports = router;
